3b2cac630f77f60f76a2a976e5f78692ba11ca6e
Malte Iwanicki <malteiwa@gmail.com>
2021-12-02 13:37:14 +0100

    Day 2

---
+++ 2/1.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+df = pd.read_csv("2/input.csv", " ", names=["order", "value"])
+
+x = 0
+y = 0
+forwards = df.loc[df["order"] == "forward", "value"].sum()
+downs = df.loc[df["order"] == "down", "value"].sum()
+ups = df.loc[df["order"] == "up", "value"].sum()
+x += forwards
+y += downs - ups
+print(f"{x*y}")
